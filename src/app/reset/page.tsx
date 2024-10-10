742083a53e87302cfacba1d1d76a7d89fe9844af
eunbeann <ds5bean23@gmail.com>
2024-10-10 15:45:15 -0700

    feat: add to HOME at reset page

--- src/app/reset/page.tsx
+++ src/app/reset/page.tsx
@@ -2,11 +2,13 @@
 
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { PersonCardProps } from "../list/components/PersonCard";
 
 export default function ResetPage() {
   const [userNames, setUserNames] = useState<PersonCardProps[]>([]);
+  const router = useRouter();
 
   useEffect(() => {
     const storedUserNames = localStorage.getItem("userNames");
@@ -64,6 +66,14 @@ export default function ResetPage() {
         >
           마지막 데이터 삭제
         </button>
+        <button
+          onClick={() => {
+            router.push("/");
+          }}
+          className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition  border-2"
+        >
+          홈으로 가기
+        </button>
       </div>
       <div className="mb-4 w-full max-w-2xl">
         <h2 className="text-xl font-semibold my-4">현재 userNames:</h2>
@@ -76,8 +86,7 @@ export default function ResetPage() {
                     <strong>ID:</strong> {user.id}
                   </span>
                   <span>
-                    <strong>이름:</strong> {user.firstName || "N/A"}{" "}
-                    {user.lastName || "N/A"}
+                    <strong>이름:</strong> {user.firstName} {user.lastName}
                   </span>
                   <span>
                     <strong>미래 이름:</strong> {user.futureFirstName}{" "}
