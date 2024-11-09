08ed38af00e8fcd4f9e56c89a59fd8dbb50f7eba
eunbeann <ds5bean23@gmail.com>
2024-11-08 16:24:31 -0800

    etc: testForReactWebcam

---
+++ src/app/camera/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import CertificationCard from "../list/components/CertificationCard";
+
+export default function CameraPage() {
+  return (
+    <CertificationCard
+      id={1729113819018}
+      date={"2024.04.24"}
+      firstName={"은빈"}
+      lastName={"권"}
+      newFirstName={"C740 BE48"}
+      newLastName={"AD8C"}
+    />
+  );
+}
