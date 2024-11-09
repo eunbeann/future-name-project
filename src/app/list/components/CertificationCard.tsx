896970b0439d6df804bb22b47f56f0d9cd52236a
eunbeann <ds5bean23@gmail.com>
2024-11-09 15:56:12 -0800

    feat: add cameta feature

--- src/app/list/components/CertificationCard.tsx
+++ src/app/list/components/CertificationCard.tsx
@@ -2,9 +2,11 @@ import certification from "@/app/assets/image/certification.png";
 import PinkButton from "@/app/common/PinkButton";
 import { format } from "date-fns";
 import { get, limitToLast, query, ref, remove } from "firebase/database";
+import Webcam from "react-webcam";
 
 import Image from "next/image";
 import Link from "next/link";
+import { useCallback, useRef, useState } from "react";
 import { db } from "../../../../firebase/firebasedb";
 
 interface CertificationCardProps {
@@ -16,6 +18,12 @@ interface CertificationCardProps {
   newLastName: string;
 }
 
+const videoConstraints = {
+  width: 103,
+  height: 156,
+  facingMode: "user",
+};
+
 export default function CertificationCard({
   id,
   date,
@@ -25,6 +33,21 @@ export default function CertificationCard({
   newLastName,
 }: CertificationCardProps) {
   const formattedDate = format(new Date(date), "MM.dd");
+
+  const webcamRef = useRef<Webcam>(null);
+  const [url, setUrl] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const handleUserMedia = () => {
+    setLoading(false);
+  };
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setUrl(imageSrc);
+    }
+  }, [webcamRef]);
 
   const deleteLastNameInArray = async () => {
     const reference = ref(db, "users");
@@ -40,11 +63,53 @@ export default function CertificationCard({
   return (
     <div className="z-50 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-dunggeunmo">
       <div className="relative">
-        <div className="bg-[#2B9EB3] absolute top-[300px] left-[119px] w-[132px] h-[204px] text-center">
-          <button className="text-[#ffffff] h-full align-text-bottom">
-            사진 촬영
-          </button>
-        </div>
+        <>
+          <div className="absolute top-[300px] left-[119px]">
+            {loading && (
+              <div className="bg-[#000000] w-[132px] h-[204px] text-center">
+                <h1 className="text-[#ffffff]">로딩중..</h1>
+              </div>
+            )}
+            <Webcam
+              className="transform scale-x-[-1]"
+              audio={false}
+              width={132}
+              height={180}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+              onUserMedia={handleUserMedia}
+              onUserMediaError={() => setLoading(false)}
+            />
+            <button
+              className="absolute bottom-[0px] ml-[11px] bg-[#ffffff] text-[14px] px-[24px]"
+              onClick={capture}
+            >
+              사진 촬영
+            </button>
+          </div>
+          {url && (
+            <div className="absolute top-[301px] left-[119px]">
+              <div>
+                <Image
+                  className="transform scale-x-[-1]"
+                  width={132}
+                  height={140}
+                  src={url}
+                  alt="Screenshot"
+                />
+              </div>
+              <button
+                className="absolute bottom-[0px] ml-[11px] bg-[#ffffff] text-[14px] px-[24px]"
+                onClick={() => {
+                  setUrl(null);
+                }}
+              >
+                다시 찍기
+              </button>
+            </div>
+          )}
+        </>
         <Image
           className="min-w-[580px] max-w-[580px] h-[855px]"
           src={certification}
