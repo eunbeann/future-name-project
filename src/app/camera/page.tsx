f0c8f0d1de7f5596c1094f876662f8715c548255
eunbeann <ds5bean23@gmail.com>
2024-11-09 17:34:22 -0800

    mock: edit test data

--- src/app/camera/page.tsx
+++ src/app/camera/page.tsx
@@ -5,7 +5,7 @@ import CertificationCard from "../list/components/CertificationCard";
 export default function CameraPage() {
   return (
     <CertificationCard
-      id={1729113819018}
+      id={2}
       date={"2024.04.24"}
       firstName={"은빈"}
       lastName={"권"}
