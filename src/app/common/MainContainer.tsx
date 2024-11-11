6bb8f8f4cf8c998caa5fd0d53019a21573c3f949
eunbeann <ds5bean23@gmail.com>
2024-11-10 22:14:52 -0800

    feat: edit mainContainer

--- src/app/common/MainContainer.tsx
+++ src/app/common/MainContainer.tsx
@@ -1,28 +1,40 @@
+import aiList from "@/app/assets/icon/aiListIcon.png";
+import code from "@/app/assets/icon/codeIcon.png";
 import headerLogo from "@/app/assets/icon/headerLogo.png";
 import headerName from "@/app/assets/icon/headerName.png";
-import Image from "next/image";
+import project from "@/app/assets/icon/projectIcon.png";
+import Image, { StaticImageData } from "next/image";
 
 export default function MainContainer({
   children,
+  bgImage,
 }: {
   children: React.ReactNode;
+  bgImage: StaticImageData;
 }) {
   return (
     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <div className="flex justify-center items-center">
-        <div className="w-[80vw] h-[80vh]">
+      <div className="flex">
+        <div className="w-[80vw] h-[80vh] relative">
           <div className="flex gap-[16px] rounded-t-[12px] bg-[#ffffff] h-[42px] pl-[30px] items-center">
             <Image src={headerLogo} alt="logo" />
             <Image src={headerName} alt="fnc.com" />
           </div>
-          <div className="h-full rounded-b-[12px] border border-[#ffffff]">
-            {children}
+          <div className="h-full rounded-b-[12px] border border-[#ffffff] relative">
+            <Image className="w-full h-full" src={bgImage} alt="lobbyBg" />
+            <div className="absolute inset-0">{children}</div>
           </div>
         </div>
-        <div>
-          <div>button1</div>
-          <div>button2</div>
-          <div>button3</div>
+        <div className="flex flex-col gap-[10px] ml-[9px]">
+          <button className="w-[78px] h-[78px]">
+            <Image src={project} alt="projectIcon" />
+          </button>
+          <button className="w-[78px] h-[78px]">
+            <Image src={code} alt="codeIcon" />
+          </button>
+          <button className="w-[78px] h-[78px]">
+            <Image src={aiList} alt="AlListIcon" />
+          </button>
         </div>
       </div>
     </div>
