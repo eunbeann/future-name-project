45ba207c02f7a6a5762d9293b417339cd97598a3
eunbeann <ds5bean23@gmail.com>
2024-11-11 16:00:12 -0800

    feat: modify lists

--- src/app/list/layout.tsx
+++ src/app/list/layout.tsx
@@ -1,38 +1,19 @@
 "use client";
 
-import fncIcon from "@/app/assets/icon/fncLogo.png";
-import listBackground from "@/app/assets/image/listBackground.png";
+import listBg from "@/app/assets/image/listBg.png";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import ClientRecoilRoot from "../common/ClientRecoilRoot";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
   return (
-    <>
+    <div className="w-[100vw] h-[100vh] bg-[#000000]">
       <Image
-        className="fixed bottom-0 w-full h-[100vh] -z-50"
-        src={listBackground}
-        alt="introBackground"
-        priority
-        draggable="false"
+        src={listBg}
+        alt="list-background-image"
+        fill
+        style={{ objectFit: "cover" }}
       />
-      <div className="relative z-10">
-        <ClientRecoilRoot>{children}</ClientRecoilRoot>
-      </div>
-      <button
-        className="fixed bottom-[80px] right-5 w-[230px] h-[190px] cursor-pointer z-20"
-        onClick={() => {
-          router.push("/");
-        }}
-      >
-        <Image
-          className="fixed bottom-[80px] right-5 w-[230px] h-[190px] cursor-pointer"
-          src={fncIcon}
-          alt="logoIcon"
-          priority
-        />
-      </button>
-    </>
+      <ClientRecoilRoot>{children}</ClientRecoilRoot>
+    </div>
   );
 }
--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,65 +1,93 @@
 "use client";
 
-import { get, ref } from "firebase/database";
+import listBackground from "@/app/assets/image/listBackground.png";
+import Image from "next/image";
 import { useEffect, useState } from "react";
-import { db } from "../../../firebase/firebasedb";
+import MainContainer from "../common/MainContainer";
 import IdentifyCard from "./components/IdentifyCard";
 import PersonCard, { PersonCardProps } from "./components/PersonCard";
 
-export default function ListPage() {
-  const [users, setUsers] = useState<PersonCardProps[]>([]);
-  const [hoveredUserId, setHoveredUserId] = useState<number | null>(null);
-
-  const data = {
-    id: 1729113819018,
+const mockUsers: PersonCardProps[] = [
+  {
+    id: 1,
     firstName: "은빈",
     lastName: "권",
     futureFirstName: "C740 BE48",
     futureLastName: "AD8C",
     date: "2024-10-16T21:23:39.018Z",
+  },
+  {
+    id: 2,
+    firstName: "민수",
+    lastName: "김",
+    futureFirstName: "A123 B456",
+    futureLastName: "C789",
+    date: "2024-10-20T18:45:12.345Z",
+  },
+];
+
+export default function ListPage() {
+  const [users, setUsers] = useState<PersonCardProps[]>([]);
+  const [hoveredUserId, setHoveredUserId] = useState<number | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 78;
+
+  const totalPages = Math.ceil(users.length / itemsPerPage);
+
+  const currentItems = users.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const handleNext = () => {
+    if (totalPages > 0 && currentPage < totalPages) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
   };
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      const reference = ref(db, "users");
-      const snapshot = await get(reference);
+    const repeatedMockUsers = Array(50)
+      .fill(mockUsers)
+      .flat()
+      .map((user, i) => ({
+        ...user,
+        id: i + 1,
+      }));
 
-      if (snapshot.exists()) {
-        const usersArray = Object.values(snapshot.val()) as PersonCardProps[];
-        setUsers(usersArray);
-      } else {
-        console.log("데이터가 없습니다.");
-        setUsers([]);
-      }
-    };
-
-    fetchUsers();
+    setUsers(repeatedMockUsers);
   }, []);
 
   return (
-    <div className="relative h-[88vh] w-[100vw] overflow-auto">
-      <div className="absolute top-0 left-0 w-full h-full z-0">
-        <div className="absolute left-1/3 h-full border-l-2 border-[#031DDF]"></div>
-        <div className="absolute left-2/3 h-full border-l-2 border-[#031DDF]"></div>
-      </div>
-      <div className="flex flex-wrap flex-col h-[88vh]">
-        {users.length > 0 ? (
-          users.map((user, key) => (
+    <MainContainer>
+      <Image
+        className="absolute -z-50 w-full h-full rounded-b-[12px]"
+        src={listBackground}
+        alt="listBackground"
+      />
+      <div className="flex flex-col flex-wrap w-[94%] h-full overflow-y-hidden py-[20px]">
+        {currentItems.length > 0 ? (
+          currentItems.map((user) => (
             <div
-              key={key}
-              onMouseEnter={() => setHoveredUserId(key)}
+              key={user.id}
+              onMouseEnter={() => setHoveredUserId(user.id)}
               onMouseLeave={() => setHoveredUserId(null)}
-              className="w-1/3 px-4"
+              className="px-4"
             >
               <PersonCard
-                id={key + 1}
+                id={user.id}
                 futureFirstName={user.futureFirstName}
                 futureLastName={user.futureLastName}
                 date={user.date}
               />
-              {hoveredUserId === key && (
+              {hoveredUserId === user.id && (
                 <IdentifyCard
-                  id={key + 1}
+                  id={user.id}
                   date={user.date}
                   firstName={user.firstName || ""}
                   lastName={user.lastName || ""}
@@ -72,7 +100,22 @@ export default function ListPage() {
         ) : (
           <p className="text-[#ffffff]">Loading....</p>
         )}
+        {currentPage === 1 ? (
+          <button
+            onClick={handleNext}
+            className="absolute right-4 bottom-1/2 w-[50px] h-[50px] bg-green9"
+          >
+            {">"}
+          </button>
+        ) : (
+          <button
+            onClick={handlePrevious}
+            className="absolute right-4 bottom-1/2 w-[50px] h-[50px] bg-green9"
+          >
+            {"<"}
+          </button>
+        )}
       </div>
-    </div>
+    </MainContainer>
   );
 }
