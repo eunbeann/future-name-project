3f3e16b1a887f1f36857aa95a50c7ba5eba7ea26
eunbeann <ds5bean23@gmail.com>
2024-11-17 12:31:43 -0800

    fix: fixing bugf

--- src/app/loading/page.tsx
+++ src/app/loading/page.tsx
@@ -4,10 +4,9 @@ import { useEffect, useState } from "react";
 import BlackPc1 from "./pages/BlackPc1";
 import BlackPc2 from "./pages/BlackPc2";
 import SkyPc1 from "./pages/SkyPc1";
-import SkyPc2 from "./pages/SkyPc2";
 
 export default function LoadingPage() {
-  const components = [SkyPc1, BlackPc1, SkyPc2, BlackPc2];
+  const components = [SkyPc1, BlackPc1, SkyPc1, BlackPc2];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -11,19 +11,30 @@ export default function ProjectPage() {
   const router = useRouter();
   return (
     <MainContainer bgImage={bg}>
-      <div className="flex flex-col == items-center h-full">
+      <div className="flex flex-col items-center h-full">
         <Image
           src={text}
           alt="FNC"
           className="absolute top-[80px] w-[340px] h-auto xl:w-[45%] xl:h-auto xl:my-[10px] z-50"
         />
         <div className="absolute top-[240px] bg-[#000000] xl:pt-[85px] xl:px-[70px] xl:w-[68%] xl:pb-[155px]">
-          <p className="tracking-wide	xl:text-[30px] text-[#ffffff] whitespace-pre-wrap font-dunggeunmo text-center">
-            {`이 프로젝트는 미래 사회를 배경으로, 인간의 이름과 정체성이 코드로 대체되는 세계를 체험하는 가상 웹사이트 형태로 구성됩니다. 사용자는 웹사이트에 접속해 가상의 "퓨처네이밍센터"에 들어가 이름을 코드로 변환하는 체험을 합니다. 도시를 탐험하고, 코드화된 사람들을 만나며, 자신의 정체성을 잃어가는 과정을 경험하게 됩니다. 탈출을 시도하지만 실패하고, 결국 자신이 시스템에 갇힌 기계임을 깨닫게 되며 이야기는 블랙코미디적 디스토피아 결말로 끝납니다.`}
+          <p className="tracking-wide	xl:text-[30px] text-[#ffffff] whitespace-pre-wrap font-dunggeunmo text-center xl:mb-[28px]">
+            {`2050년의 세상은 인공지능이 인간의 삶을 모든 측면에서 지배하는 시대입니다.
+정부, 경제, 교육, 모든 시스템이 AI에 의해 운영되며,
+인간은 그 안에서 효율적인 단위로 변환되었습니다.
+이름 역시 그 흐름의 일환으로, 더 이상 감성적이거나 문화적인 요소가 아닌
+인공지능 시스템이 이해하기 쉬운 코드로 변화했습니다.
+
+미래 사회에서는 개명도 유니코드 시스템에 따라 철저히 관리되며,
+이름이 아닌 코드로 개인의 정체성이 확립됩니다.
+퓨처네이밍센터에서는 이러한 변화 속에서 어떻게
+자신의 정체성을 새롭게 정의할 수 있는지를 탐구합니다.
+이 웹사이트를 통해 사용자는 미래 사회의 흐름을 반영한 이름을 받으며,
+자신을 새롭게 정립하는 체험을 할 수 있습니다.`}
           </p>
           <button
             onClick={() => router.back()}
-            className="absolute bottom-[18px] flex justify-center xl:w-[90%] xl:h-auto xl:my-[10px] z-50"
+            className="absolute bottom-[22px] flex justify-center xl:w-[90%] xl:h-auto xl:my-[16px] z-50"
           >
             <PinkButton text="뒤로가기" />
           </button>
