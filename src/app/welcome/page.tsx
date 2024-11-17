f98126d083b545b3c507a7aa504a0475d9295a72
eunbeann <ds5bean23@gmail.com>
2024-11-17 12:19:06 -0800

    fix: add arrow

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,12 +1,30 @@
+"use client";
+
+import arrow from "@/app/assets/gif/movingArrow.gif";
 import text from "@/app/assets/image/welcomeTxt.png";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function WelcomePage() {
+  const router = useRouter();
   return (
-    <Image
-      className="w-[100vw] h-[100vh] object-cover z-30"
-      src={text}
-      alt="text"
-    />
+    <>
+      <Image
+        className="w-[100vw] h-[100vh] object-cover z-30"
+        src={text}
+        alt="text"
+      />
+      <button
+        onClick={() => {
+          router.back();
+        }}
+      >
+        <Image
+          className="absolute left-[54px] bottom-[37px] xl:w-[98px] "
+          src={arrow}
+          alt="arrow"
+        />
+      </button>
+    </>
   );
 }
