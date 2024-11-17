0e63180d015a2ca4546e25d2f169936a40d87c52
eunbeann <ds5bean23@gmail.com>
2024-11-16 20:21:24 -0800

    chore: change init page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,9 @@
-"use client";
+import WhitePc from "./loading/pages/WhitePc";
 
-import movingLogo from "@/app/assets/gif/movingLogo.gif";
-import archiveIcon from "@/app/assets/icon/archiveIcon.png";
-import helloIcon from "@/app/assets/icon/helloIcon.png";
-import renameIcon from "@/app/assets/icon/renamingIcon.png";
-import introBackground from "@/app/assets/image/mainBg.png";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import TaskBar from "./common/TaskBar";
-
-export default function Home() {
-  const router = useRouter();
+export default function LoadingPage() {
   return (
-    <>
-      <div className="flex flex-col gap-y-[20px] pl-[45px] pt-[40px]">
-        <button className="w-[134px]" onClick={() => router.push("/intro")}>
-          <Image src={helloIcon} alt="helloIcon" />
-        </button>
-        <button className="w-[134px]" onClick={() => router.push("/person")}>
-          <Image src={renameIcon} alt="renameIcon" />
-        </button>
-        <button className="w-[134px]" onClick={() => router.push("/list")}>
-          <Image src={archiveIcon} alt="archiveIcon" />
-        </button>
-      </div>
-      <Image
-        className="relative h-[100vh] object-full -z-10"
-        src={introBackground}
-        alt="introBackground"
-        fill
-        draggable="false"
-      />
-
-      <Image
-        src={movingLogo}
-        alt="설명 텍스트"
-        className="absolute w-[480px] h-auto left-1/2 top-1/2 z-50 transform -translate-x-1/2 -translate-y-1/2"
-        priority
-      />
-      <TaskBar />
-    </>
+    <div>
+      <WhitePc />
+    </div>
   );
 }
