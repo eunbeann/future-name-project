4bbd36de9d674b6ccdcf63d456bf4572bd2c0b8b
eunbeann <ds5bean23@gmail.com>
2024-11-26 17:08:01 -0800

    feat: add navigating

--- src/app/sending/steps/Sending3.tsx
+++ src/app/sending/steps/Sending3.tsx
@@ -3,8 +3,10 @@
 import sending from "@/app/assets/gif/sending1Img.gif";
 import NeonDialog from "@/app/common/NeonDialog";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Sending3() {
+  const router = useRouter();
   return (
     <div className="relative flex justify-center items-center">
       <div className="relative xl:w-[90%] rounded-lg overflow-hidden">
@@ -24,7 +26,12 @@ export default function Sending3() {
       </div>
 
       <div className="absolute -bottom-[76px] flex justify-center xl:w-[100%]">
-        <NeonDialog sending>
+        <NeonDialog
+          sending
+          action={() => {
+            router.push("/archive");
+          }}
+        >
           <p className="font-dunggeunmo text-[18px] xl:text-[32px] text-[#000000] absolute right-[98px] bottom-[28px]">
             아카이브센터로 가기
           </p>
