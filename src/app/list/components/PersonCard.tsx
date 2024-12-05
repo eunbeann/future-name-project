a289b77681441c9aeb4e40e0429176fe1279deb2
eunbeann <ds5bean23@gmail.com>
2024-12-05 15:25:42 -0800

    design: ratio for mac

--- src/app/list/components/PersonCard.tsx
+++ src/app/list/components/PersonCard.tsx
@@ -21,12 +21,12 @@ export default function PersonCard({
   const padId = String(id).padStart(3, "0");
 
   return (
-    <div className="flex font-pixardisplay justify-between text-white hover:bg-white hover:text-[#031DDF] xl:text-[26px] mr-[3rem] xl:mr-[4rem] mb-[8px] xl:w-[457px] w-[339px]">
+    <div className="flex font-pixardisplay justify-between text-white hover:bg-white hover:text-[#031DDF] xl:text-[28px] mr-[3rem] xl:mr-[4rem] mb-[8px] xl:w-[587px] w-[339px]">
       <div className="flex">
         <p className="mr-[24px] text-[#ffffff] ">{padId}</p>
-        <p className="text-[#ffffff] xl:mr-[3rem] overflow-x-scroll scrollbar-hide">{`<${futureLastName} ${futureFirstName}>`}</p>
+        <p className="text-[#ffffff] xl:min-w-[230px] xl:mr-[3rem] overflow-x-scroll scrollbar-hide">{`<${futureLastName} ${futureFirstName}>`}</p>
       </div>
-      <p className="text-[#031DDF]">{time}</p>
+      <p className="text-[#031DDF] xl:min-w-[124px]">{time}</p>
     </div>
   );
 }
