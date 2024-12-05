b42faf457935ce5132e95c0042c0adfc0ca828f4
eunbeann <ds5bean23@gmail.com>
2024-12-05 15:54:02 -0800

    design: change ratio for pad

--- src/app/list/components/IdentifyCard.tsx
+++ src/app/list/components/IdentifyCard.tsx
@@ -33,19 +33,19 @@ export default function IdentifyCard({
               height={203}
               src={img}
               alt="X"
-              className="absolute top-[103px] right-[91px] h-[203px] xl:h-[320px] xl:w-[220px] xl:top-[155px] xl:right-[136px] scale-x-[-1]"
+              className="absolute top-[89px] right-[77px] h-[183px] w-[129px] xl:h-[320px] xl:w-[220px] xl:top-[155px] xl:right-[136px] scale-x-[-1]"
             />
-            <p className="absolute text-center w-[300px] xl:w-[480px] top-[132px] xl:top-[200px] right:[270px] xl:left-[120px] text-[32px] text-[#ffffff] xl:text-[48px]">
+            <p className="absolute text-center w-[300px] right-[220px] top-[127px] xl:w-[480px] xl:top-[200px] xl:right:[270px] xl:left-[120px] text-[32px] text-[#ffffff] xl:text-[48px]">
               {`${futureLastName} ${futureFirstName}`}
             </p>
-            <p className="absolute text-center w-[300px] xl:w-[420px] top-[168px] xl:top-[275px] right-[270px] xl:left-[150px] text-xl xl:text-[40px] text-[#ffffff] ">
+            <p className="absolute text-center w-[300px] right-[220px] xl:w-[420px] left top-[168px] xl:top-[275px] xl:left-[150px] text-xl xl:text-[40px] text-[#ffffff] ">
               {lastName}
               {firstName}
             </p>
-            <p className="absolute text-center xl:text-[24px] w-[66px] xl:w-[97px] bottom-[153px] xl:bottom-[236px] right-[308px] xl:right-[474px] text-[#ffffff]">
+            <p className="absolute text-center  xl:text-[24px] w-[66px] xl:w-[97px] bottom-[135px] xl:bottom-[236px] right-[268px] xl:right-[474px] text-[#ffffff]">
               {id}
             </p>
-            <p className="absolute text-center xl:text-[24px] w-[92px] xl:w-[128px] bottom-[118px] xl:bottom-[188px] right-[307px] xl:right-[474px]  text-[#ffffff]">
+            <p className="absolute text-center xl:text-[24px] w-[92px] xl:w-[128px] bottom-[108px] xl:bottom-[188px] right-[267px] xl:right-[474px]  text-[#ffffff]">
               2050.{formattedDate}
             </p>
           </>
