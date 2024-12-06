d76b89c9bfeaaa6941e7efe658da9dfb8f11ba21
eunbeann <ds5bean23@gmail.com>
2024-12-06 14:33:02 -0800

    desing: resizing IdentifyCard

--- src/app/list/components/IdentifyCard.tsx
+++ src/app/list/components/IdentifyCard.tsx
@@ -33,19 +33,19 @@ export default function IdentifyCard({
               height={203}
               src={img}
               alt="X"
-              className="absolute top-[89px] right-[77px] h-[183px] w-[129px] xl:h-[320px] xl:w-[220px] xl:top-[155px] xl:right-[136px] scale-x-[-1]"
+              className="absolute top-[80px] right-[70px] h-[170px] w-[117px] xl:h-[320px] xl:w-[220px] xl:top-[155px] xl:right-[136px] scale-x-[-1]"
             />
-            <p className="absolute text-center w-[300px] right-[220px] top-[127px] xl:w-[480px] xl:top-[200px] xl:right:[270px] xl:left-[120px] text-[32px] text-[#ffffff] xl:text-[48px]">
+            <p className="absolute text-center w-[240px] right-[215px] top-[105px] xl:w-[480px] xl:top-[200px] xl:left-[120px] text-[32px] text-[#ffffff] xl:text-[48px]">
               {`${futureLastName} ${futureFirstName}`}
             </p>
-            <p className="absolute text-center w-[300px] right-[220px] xl:w-[420px] left top-[168px] xl:top-[275px] xl:left-[150px] text-xl xl:text-[40px] text-[#ffffff] ">
+            <p className="absolute text-center w-[230px] right-[220px] top-[148px] text-xl xl:w-[420px] xl:top-[275px] xl:left-[150px] xl:text-[40px] text-[#ffffff] ">
               {lastName}
               {firstName}
             </p>
-            <p className="absolute text-center  xl:text-[24px] w-[66px] xl:w-[97px] bottom-[135px] xl:bottom-[236px] right-[268px] xl:right-[474px] text-[#ffffff]">
+            <p className="absolute text-center w-[56px] bottom-[123px] right-[250px] xl:text-[24px] xl:bottom-[236px] xl:right-[494px] text-[#ffffff]">
               {id}
             </p>
-            <p className="absolute text-center xl:text-[24px] w-[92px] xl:w-[128px] bottom-[108px] xl:bottom-[188px] right-[267px] xl:right-[474px]  text-[#ffffff]">
+            <p className="absolute text-center bottom-[93px] right-[237px] text-[14px] xl:text-[24px] w-[92px] xl:w-[128px] xl:bottom-[183px] xl:right-[474px] text-[#ffffff]">
               2050.{formattedDate}
             </p>
           </>
@@ -55,7 +55,7 @@ export default function IdentifyCard({
         <div className="w-full text-center font-dunggeunmo">
           <Link
             href={"/sending"}
-            className="inline-block w-[500px] xl:w-[700px] py-[16px] xl:text-[26px] bg-[#02FE00] rounded-[22px] border border-[#ffffff] "
+            className="inline-block w-[350px] py-[8px] rounded-[12px] bg-[#02FE00] xl:py-[16px] xl:rounded-[22px] border border-[#ffffff] xl:w-[700px] xl:text-[26px] "
           >
             데이터 전송하기
           </Link>
