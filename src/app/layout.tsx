97128d3b871666e270886fc42ad0c04ea9fef26b
eunbeann <ds5bean23@gmail.com>
2025-12-21 13:09:14 -0600

    feat: apply ScaledContainer to root layout
    - Wrap Header and children with ScaledContainer
    - Enable global scaling for all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import localfont from "next/font/local";
 import Header from "./common/Header";
+import ScaledContainer from "./common/ScaledContainer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -39,12 +40,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      {/* <body className={inter.className}> */}
       <body
         className={`${inter.className} ${pixardisplay.variable} ${dunggeunmo.variable} `}
       >
-        <Header />
-        {children}
+        <ScaledContainer>
+          <Header />
+          {children}
+        </ScaledContainer>
       </body>
     </html>
   );
